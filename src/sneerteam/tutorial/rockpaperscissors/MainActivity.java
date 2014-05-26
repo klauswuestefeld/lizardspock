834ce6aac4e8db30b6d589a32cd9020d268b27f6
Felipe Bueno <bueno.felipe@gmail.com>
2014-05-26 09:03:09 -0300

    strings, menu and adding game loop

--- src/sneerteam/tutorial/rockpaperscissors/MainActivity.java
+++ src/sneerteam/tutorial/rockpaperscissors/MainActivity.java
@@ -1,15 +1,12 @@
 package sneerteam.tutorial.rockpaperscissors;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -18,47 +15,49 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	    builder.setTitle("Choose your move");
+	    
+	    builder.setTitle("Choose your move").setItems(new CharSequence[]
+	            {"Rock", "Paper", "Scissors"},
+	            new DialogInterface.OnClickListener() {
+	                public void onClick(DialogInterface dialog, int which) {
+	                    switch (which) {
+	                        case 0:
+	                        	toast("choose rock");
+	                            break;
+	                        case 1:
+	                        	toast("choose paper");
+	                            break;
+	                        case 2:
+	                        	toast("choose scissors");
+	                            break;
+	                    }
+	                }
+	            });
+	    builder.create().show();
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_newGame) {
+			return true;
+		} else if (id == R.id.action_invite) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+	}	
+	
+	private void toast(String msg) {
+		Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
 	}
 
 }
