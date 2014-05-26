e85d4134f978f19536970cf60ee83ccab82fddd9
Felipe Bueno <bueno.felipe@gmail.com>
2014-05-26 09:09:25 -0300

    added Cloud class

---
+++ src/sneerteam/tutorial/rockpaperscissors/RockPaperScissorsCloud.java
@@ -0,0 +1,25 @@
+package sneerteam.tutorial.rockpaperscissors;
+
+public class RockPaperScissorsCloud {
+	 
+	 public class PublicKey {} //Trocar isso por uma classe PublicKey na Snapi. Networker já tem uma, vale a pena usar a mesma?
+
+	 public interface MoveCallback {
+	  void handle(Move move);
+	 }
+
+	 public enum Move { ROCK, PAPER, SCISSORS }
+
+	 public PublicKey pickAdversary() {
+	  return null;
+	 }
+
+	 public String nameFor(PublicKey adversary) {
+	  return "Neide";
+	 }
+
+	 public void moveAgainst(PublicKey adversary, Move move, MoveCallback onReply) {
+	  onReply.handle(Move.ROCK);
+	 }
+
+	}
