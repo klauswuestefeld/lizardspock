6e6ee8457eb54b830d992a1d721b9e50b007583b
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-05-27 17:37:10 -0300

    Minor

--- src/sneerteam/tutorial/rockpaperscissors/RockPaperScissors.java
+++ src/sneerteam/tutorial/rockpaperscissors/RockPaperScissors.java
@@ -4,29 +4,29 @@ import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 
-public class RockPaperScissors {
+class RockPaperScissors {
 
 	static boolean TUTORIAL_MODE = false;
 
-	public class Adversary {
+	class Adversary {
 		@Override
 		public String toString() {
 			return "Neide";
 		}
 	}
 	
-	public interface MoveCallback {
+	interface MoveCallback {
 		void handle(Move move);
 	}
 
-	public enum Move { ROCK, PAPER, SCISSORS }
+	enum Move { ROCK, PAPER, SCISSORS }
 
 	
-	public Observable<Adversary> pickAdversary() {
+	Observable<Adversary> pickAdversary() {
 		return Observable.from(new Adversary());
 	}
 	
-	public Observable<Move> moveAgainst(Adversary adversary) {
+	Observable<Move> moveAgainst(Adversary adversary) {
 		return Observable.from(Move.ROCK).delay(1000l, TimeUnit.MILLISECONDS);
 	}
 
