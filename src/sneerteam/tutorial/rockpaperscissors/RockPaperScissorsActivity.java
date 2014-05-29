f206ef92e5647ec6a22daceae7cad790e9689158
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-05-29 08:27:24 -0300

    Minor

--- src/sneerteam/tutorial/rockpaperscissors/RockPaperScissorsActivity.java
+++ src/sneerteam/tutorial/rockpaperscissors/RockPaperScissorsActivity.java
@@ -24,51 +24,45 @@ public class RockPaperScissorsActivity extends Activity {
 	private String adversary;
 	private Move move;
 	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);		
 
-		Button button = (Button)findViewById(R.id.btnNewGame);
-		button.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View v) {
+		int rename_btnNewGame_to_btnChallenge;
+		button(R.id.btnNewGame).setOnClickListener(new View.OnClickListener() { @Override public void onClick(View v) {
 			challenge();
 		}});
 		
 		cloud = Cloud.cloudFor(this);	   
 		
-		cloud.path(":me", "contacts").children().subscribe(new Action1<PathEvent>() {
- 			@Override
- 			public void call(PathEvent contact) {
- 				final String contactKey = (String) contact.path().lastSegment();
- 				cloud.path(contactKey, GAME, ":me", EVENT).value().cast(String.class).observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<String>() {	
- 					@Override
- 					public void call(String id) { 						
- 						AlertDialog alertDialog = new AlertDialog.Builder(RockPaperScissorsActivity.this)
- 						.setTitle("invite from " + contactKey)
- 						.setNegativeButton("Cancel", null)
- 						.setPositiveButton("Ok", new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int id) { 							 							
- 							chooseMove();
- 							cloud.path(contactKey, GAME, ROOM, id).value().subscribe(new Action1<Object>() {
- 								@Override
- 								public void call(Object event) {
- 									toast((String) event);
- 									// do stuff
- 								}
- 							});
- 						}})
- 						.create();
- 					alertDialog.show();
- 					}
- 				});
- 			}
- 		});		
-	}   
+		cloud.path(":me", "contacts").children().subscribe(new Action1<PathEvent>() { @Override public void call(PathEvent contact) {
+			final String contactKey = (String) contact.path().lastSegment();
+			cloud.path(contactKey, GAME, ":me", EVENT).value().cast(String.class).observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<String>() { @Override public void call(String id) {
+				int makeThisIntoANotificationInsteadOfAnAlert;
+				new AlertDialog.Builder(RockPaperScissorsActivity.this)
+					.setTitle("Challenge from " + contactKey)
+					.setNegativeButton("Cancel", null)
+					.setPositiveButton("OK", new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int id) {
+						chooseMove();
+						cloud.path(contactKey, GAME, ROOM, id).value().subscribe(new Action1<Object>() { @Override public void call(Object event) {
+							toast((String) event);
+							// do stuff
+						}});
+					}})
+					.show();
+			}});
+		}});		
+	}
+
 
 	private void challenge() {
   		Intent intent = new Intent("sneerteam.intent.action.PICK_CONTACT");
   		startActivityForResult(intent, PICK_CONTACT_REQUEST);
   	}	
   	
+	
   	@Override
   	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
   		super.onActivityResult(requestCode, resultCode, intent);
@@ -81,55 +75,48 @@ public class RockPaperScissorsActivity extends Activity {
 		String id = UUID.randomUUID().toString();
 		cloud.path(GAME, adversary, EVENT).pub(id);
 		
-		cloud.path(adversary, GAME, ROOM, id).value().subscribe(new Action1<Object>() {
-			@Override
-			public void call(Object event) {
-				toast((String) event);
-				// do stuff
-			}
-		}); 
+		cloud.path(adversary, GAME, ROOM, id).value().subscribe(new Action1<Object>() { @Override public void call(Object event) {
+			toast((String) event);
+			// do stuff
+		}}); 
   	}
 
+  	
 	private void chooseMove() {
 		move = null;
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Choose your move against " + adversary);
-		builder.setItems(new CharSequence[] 
-		{"Rock", "Paper", "Scissors"},
-		new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				move = Move.values()[which];					
-				waitForAdversary();
-			}
-		});
-		builder.create().show();
+		new AlertDialog.Builder(this)
+			.setTitle("Choose your move against " + adversary)
+			.setItems(
+				new CharSequence[] {"Rock", "Paper", "Scissors"},
+				new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int which) {
+					move = Move.values()[which];					
+					waitForAdversary();
+				}}
+			).show();
 	}
+	
 	
 	private void waitForAdversary() {
 		final ProgressDialog waiting = ProgressDialog.show(this, null, "Waiting for " + adversary + "...");
 		waiting.setIndeterminate(true);
 		waiting.setCancelable(true);		
-		rps.moveAgainst(adversary).observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<Move>() {
-			@Override
-			public void call(Move reply) {
-				waiting.dismiss();
-				onReply(reply);
-			}
-		});
+		rps.moveAgainst(adversary).observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<Move>() { @Override public void call(Move reply) {
+			waiting.dismiss();
+			onReply(reply);
+		}});
 	}
+	
 	
 	private void onReply(Move reply) {
 		String result = result(reply);				
 		String message = "You used " + move + ". " + adversary + " used " + reply + ".";
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle(result).setMessage(message).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
+		new AlertDialog.Builder(this)
+			.setTitle(result).setMessage(message).setPositiveButton("OK", new DialogInterface.OnClickListener() { @Override public void onClick(DialogInterface dialog, int which) {
 				playAgain();
-			}
-		}).show();
+			}}).show();
 	}
+	
 	
 	private String result(Move reply) {
 		if (move == reply) return "Draw!";
@@ -140,6 +127,7 @@ public class RockPaperScissorsActivity extends Activity {
 		
 		return "You lose";
 	}	
+	
 	
 	private void playAgain() {
 		DialogInterface.OnClickListener chooseMove = new DialogInterface.OnClickListener() {
@@ -156,8 +144,14 @@ public class RockPaperScissorsActivity extends Activity {
 		builder.show();
 	}
 	
+	
 	private void toast(String msg) {
 		Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
 	}
+
 	
+	private Button button(int id) {
+		return (Button)findViewById(id);
+	}   
 }
+
