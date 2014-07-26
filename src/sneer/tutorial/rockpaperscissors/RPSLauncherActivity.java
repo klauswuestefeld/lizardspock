b7b3aa87da449637c0d5df36959ad338d6790bd7
Fabio Roger Manera <fabioroger@gmail.com>
2014-07-26 16:15:48 +0100

    add label for new instance challenge and simplify nickname access

--- src/sneer/tutorial/rockpaperscissors/RPSLauncherActivity.java
+++ src/sneer/tutorial/rockpaperscissors/RPSLauncherActivity.java
@@ -2,19 +2,19 @@ package sneer.tutorial.rockpaperscissors;
 
 import sneer.*;
 import android.app.*;
-import android.os.*;
 
 public class RPSLauncherActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected void onResume() {
+		super.onResume();
 		
 		String title = "RPS Challenges";
 		String type = "rock-paper-scissor/move";
+		String newInteractionLabel = "Challenge!!";
 		String newInteractionAction = "sneer.tutorial.rockpaperscissors.CHALLENGE";
 		
-		SneerAndroid.startInteractionList(this, title, type, newInteractionAction);
+		SneerAndroid.startInteractionList(this, title, type, newInteractionLabel, newInteractionAction);
 	}
 
 }
