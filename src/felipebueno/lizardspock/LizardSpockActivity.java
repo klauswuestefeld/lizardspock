43db1e00b0a4a077ac916e2e68daa0411e26b42a
Felipe Bueno <bueno.felipe@gmail.com>
2014-11-17 12:34:37 -0200

    minor

--- src/felipebueno/lizardspock/LizardSpockActivity.java
+++ src/felipebueno/lizardspock/LizardSpockActivity.java
@@ -1,9 +1,14 @@
 package felipebueno.lizardspock;
 
-import static felipebueno.lizardspock.LizardSpockActivity.Move.*;
-import sneer.android.ui.*;
-import android.app.*;
-import android.content.*;
+import static felipebueno.lizardspock.LizardSpockActivity.Move.LIZARD;
+import static felipebueno.lizardspock.LizardSpockActivity.Move.PAPER;
+import static felipebueno.lizardspock.LizardSpockActivity.Move.ROCK;
+import static felipebueno.lizardspock.LizardSpockActivity.Move.SCISSORS;
+import static felipebueno.lizardspock.LizardSpockActivity.Move.SPOCK;
+import sneer.android.ui.PartnerSessionActivity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 
 public class LizardSpockActivity extends PartnerSessionActivity {
@@ -17,38 +22,38 @@ public class LizardSpockActivity extends PartnerSessionActivity {
 	private Move adversarysMove;
 	private ProgressDialog waitingForAdversarysMove;
 
-	
+
 	@Override
 	protected void onPartnerName(String name) {
 		adversary = name;
 	}
-	
-	
+
+
 	@Override
 	protected void onMessageToPartner(Object message) {
 		yourMove = Move.valueOf((String)message);
 	}
 
-	
+
 	@Override
 	protected void onMessageFromPartner(Object message) {
 		adversarysMove = Move.valueOf((String)message);
 	}
 
-	
+
 	@Override
 	protected void update() {
 		if (yourMove == null) {
 			waitForYourMove();
 			return;
 		}
-		
+
 		if (adversarysMove == null) {
 			waitingForAdversarysMove = progressDialog("Waiting for " + adversary + "...");
 			return;
 		}
 		if (waitingForAdversarysMove != null) waitingForAdversarysMove.dismiss();
-		
+
 		gameOver();
 	}
 
@@ -56,16 +61,17 @@ public class LizardSpockActivity extends PartnerSessionActivity {
 	private void waitForYourMove() {
 		if (waitingForYourMove) return;
 		waitingForYourMove = true;
-		
-		alert("Choose Your Move", options("Rock", "Paper", "Scissors", "Lizard", "Spock"), new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int option) {
+
+		alert("Choose Your Move", options("Rock", "Paper", "Scissors", "Lizard", "Spock"), new DialogInterface.OnClickListener() { @Override
+		public void onClick(DialogInterface dialog, int option) {
 			String move = Move.values()[option].name();
 			send("Lizard Spock Challenge!", move);
 		}});
 	}
-	
+
 
 	private void gameOver() {
-		String outcome = outcome();				
+		String outcome = outcome();
 		String message = "You used " + yourMove + ". " + adversary + " used " + adversarysMove + ".";
 
 		alert(outcome + " " + message, options("OK"), new DialogInterface.OnClickListener() { @Override public void onClick(DialogInterface dialog, int which) {
@@ -79,16 +85,16 @@ public class LizardSpockActivity extends PartnerSessionActivity {
 
 		if (yourMove == ROCK     && adversarysMove == SCISSORS) return "You win!";
 		if (yourMove == ROCK     && adversarysMove == LIZARD  ) return "You win!";
-		
+
 		if (yourMove == SCISSORS && adversarysMove == PAPER   ) return "You win!";
 		if (yourMove == SCISSORS && adversarysMove == LIZARD  ) return "You win!";
-		
+
 		if (yourMove == PAPER    && adversarysMove == ROCK    ) return "You win!";
 		if (yourMove == PAPER    && adversarysMove == SPOCK   ) return "You win!";
-		
+
 		if (yourMove == LIZARD   && adversarysMove == SPOCK   ) return "You win!";
 		if (yourMove == LIZARD   && adversarysMove == PAPER   ) return "You win!";
-		
+
 		if (yourMove == SPOCK    && adversarysMove == ROCK    ) return "You win!";
 		if (yourMove == SPOCK    && adversarysMove == SCISSORS) return "You win!";
 
@@ -102,11 +108,12 @@ public class LizardSpockActivity extends PartnerSessionActivity {
 		ret.setCancelable(true);
 		ret.setOnCancelListener(new OnCancelListener() {  @Override public void onCancel(DialogInterface dialog) {
 			finish();
-		} });
+		}});
 		return ret;
 	}
 
 
+	@Override
 	protected void alert(String title, CharSequence[] items, DialogInterface.OnClickListener onClickListener) {
 		new AlertDialog.Builder(this)
 			.setTitle(title)
@@ -114,7 +121,7 @@ public class LizardSpockActivity extends PartnerSessionActivity {
 			.show()
 			.setOnCancelListener(new OnCancelListener() {  @Override public void onCancel(DialogInterface dialog) {
 				finish();
-			} });;
+			}});;
 	}
 
 
